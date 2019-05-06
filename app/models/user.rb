4033bed4b4e183a811ccb8f925f3c3b767eea45b
Dmitry <dimaandreev90@gmail.com>
2019-05-06 22:46:16 +0300

    initial commit lesson 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def tests_by_level(test_level)
+    Test.joins('join results on results.test_id = tests.id')
+        .where(tests: { level: test_level }, results: { user_id: id } )
+  end
+
 end
